1a1d8541051075e1a176f046f3d0571655d6f8c3
arya <aryaa.hdyt@gmail.com>
2022-10-06 21:29:21 +0700

    update

--- js/menikah.js
+++ js/menikah.js
@@ -272,28 +272,28 @@ $.fn.serializeObject = function()
 //   Body.addClass("preloader-site");
 // });
 
- function onYouTubeIframeAPIReady() {
-  var player;
-  player = new YT.Player('muteYouTubeVideoPlayer', {
-    videoId: 'fXN-m49MHO0', // YouTube Video ID
-    width: 560,               // Player width (in px)
-    height: 316,              // Player height (in px)
-    playerVars: {
-      autoplay: 1,        // Auto-play the video on load
-      controls: 0,        // Show pause/play buttons in player
-      showinfo: 0,        // Hide the video title
-      modestbranding: 1,  // Hide the Youtube Logo
-      loop: 1,            // Run the video in a loop
-      fs: 0,              // Hide the full screen button
-      cc_load_policy: 1, // Hide closed captions
-      iv_load_policy: 3,  // Hide the Video Annotations
-      autohide: 0,         // Hide video controls when playing
-      rel: 0,
-    },
-    events: {
-      onReady: function(e) {
-        e.target.mute();
-      }
-    }
-  });
- }
+//  function onYouTubeIframeAPIReady() {
+//   var player;
+//   player = new YT.Player('muteYouTubeVideoPlayer', {
+//     videoId: 'fXN-m49MHO0', // YouTube Video ID
+//     width: 560,               // Player width (in px)
+//     height: 316,              // Player height (in px)
+//     playerVars: {
+//       autoplay: 1,        // Auto-play the video on load
+//       controls: 0,        // Show pause/play buttons in player
+//       showinfo: 0,        // Hide the video title
+//       modestbranding: 1,  // Hide the Youtube Logo
+//       loop: 1,            // Run the video in a loop
+//       fs: 0,              // Hide the full screen button
+//       cc_load_policy: 1, // Hide closed captions
+//       iv_load_policy: 3,  // Hide the Video Annotations
+//       autohide: 0,         // Hide video controls when playing
+//       rel: 0,
+//     },
+//     events: {
+//       onReady: function(e) {
+//         e.target.mute();
+//       }
+//     }
+//   });
+//  }
